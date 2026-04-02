1e2c992a78738558b8573eaf730fefa066038608
Lin Yang <coding@lyang.dev>
2026-04-01 21:58:16 -0700

    Add MTI and bitmap field specs with codec builder to CMFMessage

--- jpos-cmf/src/main/java/io/bytestreams/codec/iso8583/examples/jpos/cmf/CMFMessage.java
+++ jpos-cmf/src/main/java/io/bytestreams/codec/iso8583/examples/jpos/cmf/CMFMessage.java
@@ -1,14 +1,27 @@
 package io.bytestreams.codec.iso8583.examples.jpos.cmf;
 
+import io.bytestreams.codec.core.Codec;
+import io.bytestreams.codec.core.Codecs;
 import io.bytestreams.codec.core.DataObject;
+import io.bytestreams.codec.core.FieldSpec;
 import io.bytestreams.codec.iso8583.Bitmapped;
+import io.bytestreams.codec.iso8583.BitmappedCodecBuilder;
+import io.bytestreams.codec.iso8583.FieldCodecs;
 import io.bytestreams.codec.iso8583.MultiBlockBitmap;
 
 public class CMFMessage extends DataObject implements Bitmapped {
-  private final MultiBlockBitmap bitmap = new MultiBlockBitmap(8);
+  public static FieldSpec<CMFMessage, String> MTI = field("mti", Codecs.hex(4));
+  public static FieldSpec<CMFMessage, MultiBlockBitmap> BITMAP =
+      field("bitmap", FieldCodecs.multiBlockBitmap(8));
+  public static Codec<CMFMessage> CODEC =
+      BitmappedCodecBuilder.builder(CMFMessage::new).field(MTI).bitmap(BITMAP).build();
+
+  public CMFMessage() {
+    BITMAP.set(this, new MultiBlockBitmap(8));
+  }
 
   @Override
   public MultiBlockBitmap getBitmap() {
-    return bitmap;
+    return BITMAP.get(this);
   }
 }
