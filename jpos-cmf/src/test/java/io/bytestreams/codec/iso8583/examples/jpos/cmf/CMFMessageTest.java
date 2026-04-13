1cecefd63e3f3cb9dc6e899accd549c2b98fa285
Lin Yang <coding@lyang.dev>
2026-04-12 23:18:52 -0700

    Add file transfer description data field (DE-070) with fixed 18-digit BCD codec

--- jpos-cmf/src/test/java/io/bytestreams/codec/iso8583/examples/jpos/cmf/CMFMessageTest.java
+++ jpos-cmf/src/test/java/io/bytestreams/codec/iso8583/examples/jpos/cmf/CMFMessageTest.java
@@ -124,6 +124,7 @@ class CMFMessageTest {
     msg.set(67, "12");
     msg.set(68, "BATCHCTL0");
     msg.set(69, "BATCH FILE TRANSFER CONTROL DATA 12345  ");
+    msg.set(70, "123456789012345678");
     ISOMsg vdMsg = new ISOMsg(49);
     vdMsg.set(2, "0123");
     vdMsg.set(3, "1234 MAIN ST    ");
@@ -291,6 +292,8 @@ class CMFMessageTest {
     assertThat(CMFMessage.BATCH_FILE_TRANSFER_MESSAGE_CONTROL.get(decoded)).isEqualTo("BATCHCTL0");
     assertThat(CMFMessage.BATCH_FILE_TRANSFER_CONTROL_DATA.get(decoded))
         .isEqualTo("BATCH FILE TRANSFER CONTROL DATA 12345  ");
+    assertThat(CMFMessage.FILE_TRANSFER_DESCRIPTION_DATA.get(decoded))
+        .isEqualTo("123456789012345678");
 
     @SuppressWarnings("unchecked")
     var inspected = (Map<String, Object>) Inspector.inspect(CMFMessage.CODEC, decoded);
@@ -310,9 +313,9 @@ class CMFMessageTest {
                 assertThat(v)
                     .hasToString(
                         "{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,"
-                            + " 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,"
-                            + " 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,"
-                            + " 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69}"))
+                            + " 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,"
+                            + " 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54,"
+                            + " 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70}"))
         .containsEntry("pan", "400012******8901")
         .containsEntry("processingCode", processingCodeMap)
         .containsEntry("transactionAmount", amountMap)
@@ -385,7 +388,8 @@ class CMFMessageTest {
         .containsEntry("amountsOriginalFees", "ORIGINALFEESDATA")
         .containsEntry("extendedPaymentData", "12")
         .containsEntry("batchFileTransferMessageControl", "BATCHCTL0")
-        .containsEntry("batchFileTransferControlData", "BATCH FILE TRANSFER CONTROL DATA 12345  ");
+        .containsEntry("batchFileTransferControlData", "BATCH FILE TRANSFER CONTROL DATA 12345  ")
+        .containsEntry("fileTransferDescriptionData", "123456789012345678");
 
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     CMFMessage.CODEC.encode(decoded, out);
@@ -491,5 +495,6 @@ class CMFMessageTest {
     assertThat(reparsed.getString(67)).isEqualTo("12");
     assertThat(reparsed.getString(68)).isEqualTo("BATCHCTL0");
     assertThat(reparsed.getString(69)).isEqualTo("BATCH FILE TRANSFER CONTROL DATA 12345  ");
+    assertThat(reparsed.getString(70)).isEqualTo("123456789012345678");
   }
 }
